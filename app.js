8495552c5169786cf2800cd2c74b2f4304a16450
hassanasif921 <51042408+hassanasif921@users.noreply.github.com>
2024-03-09 03:32:40 +0500

    Update app.js

--- app.js
+++ app.js
@@ -13,7 +13,7 @@ mongoose.connect(process.env.MONGO_URI, {
     useNewUrlParser: true,
     useUnifiedTopology: true,
 });
-
+console.log(process.env.MONGO_URI)
 const db = mongoose.connection;
 db.on('error', console.error.bind(console, 'connection error:'));
 db.once('open', () => {
