bb903445c5f647c7b3133888e8682863a1d482e4
Ammar Siddiqui <126758604+Ammar-Hussain-Siddiqui@users.noreply.github.com>
2024-03-11 11:08:19 +0500

    Update app.js

--- app.js
+++ app.js
@@ -10,14 +10,14 @@ const app = express();
 const port = process.env.PORT;
 
 mongoose.connect(process.env.MONGO_URI, {
-useNewUrlParser: true,
-useUnifiedTopology: true,
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
 });
 
 const db = mongoose.connection;
 db.on('error', console.error.bind(console, 'connection error:'));
 db.once('open', () => {
-console.log('Connected to MongoDB');
+    console.log('Connected to MongoDB');
 });
 
 const MAX_REQUESTS_PER_MINUTE = 60; 
